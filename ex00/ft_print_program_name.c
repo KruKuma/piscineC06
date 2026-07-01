d5c384ad4280165c2301af1bd2fbcd914efaf2c9
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-01 20:25:14 +0200

    feat(ex00): add a function to print program name

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/01 15:45:25 by nfurst            #+#    #+#             */
+/*   Updated: 2026/07/01 15:45:25 by nfurst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_program_name(char *argv[])
+{
+    int	i;
+
+    i = 0;
+    while (argv[0][i] != '\0')
+    {
+        write(1, &argv[0][i], 1);
+        i++;
+    }
+    write(1, "\n", 1);
+}
+
+int	main(int argc, char *argv[])
+{
+    (void)argc; // Unused parameter
+    ft_print_program_name(argv);
+    return (0);
+}
