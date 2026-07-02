b2ffabaf5eb63317650158dbeaf43a5b4d762061
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-02 12:16:08 +0200

    fix(ex01): fix formatting for norminette

--- ex01/ft_print_params.c
+++ ex01/ft_print_params.c
@@ -6,33 +6,33 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 20:26:00 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/01 20:26:00 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/07/02 12:15:09 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_print_params(int argc, char *argv[])
+void	ft_print_params(int argc, char *argv[])
 {
-    int	i;
-    int	j;
+	int	i;
+	int	j;
 
-    i = 1; // Start from 1 to skip the program name
-    while (i < argc)
-    {
-        j = 0;
-        while (argv[i][j] != '\0')
-        {
-            write(1, &argv[i][j], 1);
-            j++;
-        }
-        write(1, "\n", 1);
-        i++;
-    }
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    ft_print_params(argc, argv);
-    return (0);
+	ft_print_params(argc, argv);
+	return (0);
 }
