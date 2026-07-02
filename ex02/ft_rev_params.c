340300835c31960d7828512272792492faa864f0
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-02 12:19:39 +0200

    fix(ex02): fix formatting for norminette

--- ex02/ft_rev_params.c
+++ ex02/ft_rev_params.c
@@ -6,7 +6,7 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 20:28:31 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/01 20:28:31 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/07/02 12:18:59 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 void	ft_rev_params(int argc, char *argv[])
 {
-    int	i;
-    int	j;
+	int	i;
+	int	j;
 
-    i = argc - 1; // Start from the last argument
-    while (i > 0) // Skip the program name
-    {
-        j = 0;
-        while (argv[i][j] != '\0')
-        {
-            write(1, &argv[i][j], 1);
-            j++;
-        }
-        write(1, "\n", 1);
-        i--;
-    }
+	i = argc - 1;
+	while (i > 0)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i--;
+	}
 }
 
 int	main(int argc, char *argv[])
 {
-    ft_rev_params(argc, argv);
-    return (0);
+	ft_rev_params(argc, argv);
+	return (0);
 }
