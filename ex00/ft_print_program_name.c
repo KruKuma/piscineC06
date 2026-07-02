9ccd7d5543fbc1d112e3406718fefe168a7226b9
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-02 12:13:48 +0200

    fix(ex00): fix formatting for norminette

--- ex00/ft_print_program_name.c
+++ ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 15:45:25 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/01 15:45:25 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/07/02 12:12:18 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 void	ft_print_program_name(char *argv[])
 {
-    int	i;
+	int	i;
 
-    i = 0;
-    while (argv[0][i] != '\0')
-    {
-        write(1, &argv[0][i], 1);
-        i++;
-    }
-    write(1, "\n", 1);
+	i = 0;
+	while (argv[0][i] != '\0')
+	{
+		write(1, &argv[0][i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
 }
 
 int	main(int argc, char *argv[])
 {
-    (void)argc; // Unused parameter
-    ft_print_program_name(argv);
-    return (0);
+	(void)argc;
+	ft_print_program_name(argv);
+	return (0);
 }
