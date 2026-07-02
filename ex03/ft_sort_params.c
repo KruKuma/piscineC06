eaecee60b95655b592a92800d8a1451e2929e70f
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-02 12:23:17 +0200

    fix(ex03): fix formatting for norminette

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 20:29:30 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/01 20:29:30 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/07/02 12:22:45 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,60 +14,60 @@
 
 int	ft_strcmp(char *s1, char *s2)
 {
-    int	i;
+	int	i;
 
-    i = 0;
-    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
-        i++;
-    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 void	ft_sort_params(int argc, char *argv[])
 {
-    int	i;
-    int	j;
-    char	*temp;
+	int		i;
+	int		j;
+	char	*temp;
 
-    i = 1; // Start from 1 to skip the program name
-    while (i < argc - 1)
-    {
-        j = i + 1;
-        while (j < argc)
-        {
-            if (ft_strcmp(argv[i], argv[j]) > 0)
-            {
-                temp = argv[i];
-                argv[i] = argv[j];
-                argv[j] = temp;
-            }
-            j++;
-        }
-        i++;
-    }
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
-void    ft_print_params(int argc, char *argv[])
+void	ft_print_params(int argc, char *argv[])
 {
-    int	i;
-    int	j;
+	int	i;
+	int	j;
 
-    i = 1; // Start from 1 to skip the program name
-    while (i < argc)
-    {
-        j = 0;
-        while (argv[i][j] != '\0')
-        {
-            write(1, &argv[i][j], 1);
-            j++;
-        }
-        write(1, "\n", 1);
-        i++;
-    }
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
 }
 
 int	main(int argc, char *argv[])
 {
-    ft_sort_params(argc, argv);
-    ft_print_params(argc, argv);
-    return (0);
+	ft_sort_params(argc, argv);
+	ft_print_params(argc, argv);
+	return (0);
 }
